b3dab96df518960106a1633ae3c9fc8593b877f0
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-19 15:55:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,10 +9,12 @@ class Shoe
     @brand = brand
     BRANDS << brand
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
 end
+
+  console.log(BRANDS)
